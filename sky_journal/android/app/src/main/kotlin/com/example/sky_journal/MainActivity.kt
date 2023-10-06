2abc530d1af9563e429cecb29cae9e058ee6f3c5
janfelber <68186567+janfelber@users.noreply.github.com>
2023-10-06 19:28:10 +0200

    navbar
    Added navbar

---
+++ sky_journal/android/app/src/main/kotlin/com/example/sky_journal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sky_journal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sky_journal/lib/homepage.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:sky_journal/pages/flights.dart';
+import 'package:sky_journal/pages/settings.dart';
+import 'package:sky_journal/pages/stats.dart';
+import 'pages/wallet.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 1;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [Wallet(), Flights(), Stats(), Settings()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Container(
+        color: const Color(0xFF01366A),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+          child: GNav(
+              backgroundColor: Color(0xFF01366A),
+              color: Colors.white,
+              activeColor: Colors.orange,
+              gap: 8,
+              selectedIndex: _selectedIndex,
+              onTabChange: _navigateBottomBar,
+              padding: EdgeInsets.all(16),
+              tabs: const [
+                GButton(
+                  icon: Icons.wallet,
+                  text: 'Cards',
+                ),
+                GButton(
+                  icon: Icons.home,
+                  text: 'Flights',
+                ),
+                GButton(
+                  icon: Icons.stacked_bar_chart,
+                  text: 'Stats',
+                ),
+                GButton(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+}
