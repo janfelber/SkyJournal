6a19dfcd6cd4169380b589ef2e7560479f20cc6f
janfelber <68186567+janfelber@users.noreply.github.com>
2023-10-08 21:59:43 +0200

    wallet
    added util to create card

--- sky_journal/lib/pages/wallet.dart
+++ sky_journal/lib/pages/wallet.dart
@@ -1,13 +1,71 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
-class Wallet extends StatelessWidget {
+import '../util/my_card.dart';
+
+class Wallet extends StatefulWidget {
   const Wallet({Key? key}) : super(key: key);
 
   @override
+  State<Wallet> createState() => _WalletState();
+}
+
+class _WalletState extends State<Wallet> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Wallet'),
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          children: [
+            //app bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'My ',
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Cards',
+                        style: TextStyle(fontSize: 28),
+                      ),
+                    ],
+                  ),
+
+                  //add card button
+                  Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: Colors.grey[400], shape: BoxShape.circle),
+                      child: Icon(Icons.add))
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+
+            //cards
+            Container(
+              height: 200,
+              child: PageView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  MyCard(),
+                  MyCard(),
+                  MyCard(),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
