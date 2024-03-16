7d654f83b5cb1e2f49fe423a704c28a2735169c8
janfelber <68186567+janfelber@users.noreply.github.com>
2024-03-16 19:18:51 +0100

    icons menu_item_airlines.dart
    added airlines logos icons in menu item airlines

--- sky_journal/lib/module/flight_module/components/menu_item_airlines.dart
+++ sky_journal/lib/module/flight_module/components/menu_item_airlines.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DropdownMenuItemsAirLines {
   static DropdownMenuItem<String> buildMenuItemAirLines(String item) =>
@@ -8,6 +9,76 @@ class DropdownMenuItemsAirLines {
           children: [
             if (item == 'Private') ...[
               Icon(Icons.star, color: Colors.white),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Air Canada') ...[
+              SvgPicture.asset(
+                'assets/airlines/Air_Canada.svg',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'British Airways') ...[
+              SvgPicture.asset(
+                'assets/airlines/British.svg',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Emirates') ...[
+              SvgPicture.asset(
+                'assets/airlines/Emirates.svg',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Etihad Airways') ...[
+              SvgPicture.asset(
+                'assets/airlines/Etihad.svg',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Japan Airlines') ...[
+              Image.asset(
+                'assets/airlines/japan-airlines_logo.png',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Lufthansa') ...[
+              SvgPicture.asset(
+                'assets/airlines/Lufthansa.svg',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Qatar Airways') ...[
+              SvgPicture.asset(
+                'assets/airlines/Qatar.svg',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Ryanair') ...[
+              Image.asset(
+                'assets/airlines/ryanair_logo.png',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Tus Airways') ...[
+              Image.asset(
+                'assets/airlines/tus-airways_logo.png',
+                height: 25,
+              ),
+              SizedBox(width: 10),
+            ],
+            if (item == 'Wizz Air') ...[
+              SvgPicture.asset(
+                'assets/airlines/Wizz_Air_UK.svg',
+                height: 25,
+              ),
               SizedBox(width: 10),
             ],
             Text(
