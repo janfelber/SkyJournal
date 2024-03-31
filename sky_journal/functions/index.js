c3551c8594768b1913e5e8f6d08439d9e1d91b13
janfelber <68186567+janfelber@users.noreply.github.com>
2024-03-31 15:55:12 +0200

    push notification function
    push notification with cloud functions Firebase

--- sky_journal/functions/index.js
+++ sky_journal/functions/index.js
@@ -2,44 +2,54 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const { DateTime } = require('luxon');
+
 exports.sendNotificationBeforeAppointment = functions.pubsub.schedule('0 0 * * *').timeZone('Europe/Bratislava').onRun(async (context) => {
+  // Get all appointments from Firestore
   const appointmentsRef = admin.firestore().collection('doctor-applications');
-  const now = admin.firestore.Timestamp.now();
+  // Get current date and format it to day month year (12 January 2024)
+  const now = DateTime.now().setZone('Europe/Bratislava').toFormat('dd LLLL yyyy');
 
   try {
-    const appointmentsSnapshot = await appointmentsRef.where('Date', '>', now).get();
+    const appointmentsSnapshot = await appointmentsRef.get();
 
     appointmentsSnapshot.forEach(async (doc) => {
+      // Get appointment data
       const appointmentData = doc.data();
+      // Get appointment date and format it to day month year (12 January 2024)
+      const appointmentDate = appointmentData.Date;
+      const appointmentDateTime = DateTime.fromMillis(appointmentDate.seconds * 1000).setZone('Europe/Bratislava').toFormat('dd LLLL yyyy');
 
-      
-      const {FcmToken, notificationSent, DoctorName, Time } = appointmentData;
+      // Check if appointment date is today
+      if (appointmentDateTime === now) {
+        // Get FCM token, notification status, doctor name and time of the appointment
+        const { FcmToken, notificationSent, DoctorName, Time } = appointmentData;
 
-      console.log('FCM token:', FcmToken);
-      console.log('Stav notifikácie:', notificationSent);
+        console.log('FCM token:', FcmToken);
+        console.log('Stav notifikácie:', notificationSent);
 
-      if (notificationSent) {
-        console.log('Notification already sent.');
-        return;
-      }
+        // Check if notification was already sent
+        if (notificationSent) {
+          console.log('Notification already sent.');
+          return;
+        }
 
-      const payload = {
-        
-        notification: {
-          //title for the notification
-          title: 'Doctor Appointment Reminder',
-          //body of the notification
-          body: 'Dr. ' + DoctorName + ' at ' + Time,
-        },
-      };
+        // Prepare notification payload
+        const payload = {
+          notification: {
+            title: 'Doctor Appointment Reminder',
+            body: 'Dr. ' + DoctorName + ' at ' + Time,
+          },
+        };
 
-      try {
-        // Send notification to the user
-        await admin.messaging().sendToDevice(FcmToken, payload);
-        await doc.ref.update({ notificationSent: true });
-        console.log('Notification was successfully sent.');
-      } catch (error) {
-        console.error('Error in sending: ', error);
+        try {
+          // Send notification to the user - FCM token is used to identify the device
+          await admin.messaging().sendToDevice(FcmToken, payload);
+          await doc.ref.update({ notificationSent: true });
+          console.log('Notification was successfully sent.');
+        } catch (error) {
+          console.error('Error in sending: ', error);
+        }
       }
     });
   } catch (error) {
