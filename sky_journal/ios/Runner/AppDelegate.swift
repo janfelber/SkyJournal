1d154caf1dff81ff30ae4a638eb388c56547057c
janfelber <68186567+janfelber@users.noreply.github.com>
2024-04-11 22:00:01 +0200

    private api key
    for security reason api key for google maps is replaced by text

--- sky_journal/ios/Runner/AppDelegate.swift
+++ sky_journal/ios/Runner/AppDelegate.swift
@@ -1,13 +1,30 @@
 import UIKit
 import Flutter
-import GoogleMaps 
+import GoogleMaps
+import UserNotifications
+import flutter_local_notifications // Make sure this is correctly imported
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBogiAHjAxIlaZmCQo6WVzihkYHagTjAdM")
+    GMSServices.provideAPIKey("YOUR_API_KEY") // add your api key here from google cloud console - https://console.cloud.google.com/
+
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+      (granted, error) in
+      if granted {
+        print("Permission granted")
+      } else {
+        print("Permission denied")
+      }
+    }
+
+    // Corrected usage of FlutterLocalNotificationsPlugin
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
 
 
     GeneratedPluginRegistrant.register(with: self)
--- sky_journal/lib/main.dart
+++ sky_journal/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -9,6 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+  FirebaseMessaging.instance.requestPermission();
 
   final prefs = await SharedPreferences.getInstance();
   final showLogin = prefs.getBool('showLogin') ?? false;
